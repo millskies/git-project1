fca30ebec09f4217e3e3bb535e16b32bc098a2d4
Mich Seixas <mcseixas@gmail.com>
2023-04-24 15:33:52 +0200

    class component added

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ class Game {
         
     }
 }
+
+
+class Component {
+    constructor(x, y, w, h, color, imgElement) {
+      this.x = x;
+      this.y = y;
+      this.width = w;
+      this.height = h;
+      this.color = color;
+      this.imgElement = imgElement;
+    }
+}
